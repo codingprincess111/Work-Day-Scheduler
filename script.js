88e23d8e2657834614c09ddc7e24dcded352f7d0
codingprincess111 <100244609+codingprincess111@users.noreply.github.com>
2023-02-24 13:52:45 -0600

    local storage

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+
 $(function () {
   var everyRow = $(".container-lg").find(".time-block");
   var currentHour = dayjs().hour()
-  var everyButtons = $(".container-lg").find(".saveBtn")
+  var everyButton = $(".container-lg").find(".saveBtn")
 
   for(var i = 0; i < everyRow.length; i++) {
     console.log(everyRow[i]);
     var idRow = $(everyRow[i]).prop('id');
     var blockTime = parseInt(idRow.slice(5));
     var currentClass = $(everyRow[i]).prop('class');
+    var savedData = localStorage.getItem("block-" + blockTime);
     if (blockTime < currentHour) {
       $(everyRow[i]).removeClass(currentClass).addClass('row time-block past')
     }
@@ -17,12 +19,21 @@ $(function () {
     if(blockTime > currentHour){
       $(everyRow[i]).removeClass(currentClass).addClass('row time-block future')
     }
+    if (savedData) {
+      $(everyRow[i]).find("textarea").val(savedData);
+    }
+
   }
+
+  everyButton.on('click', function() {
+    var timeBlock = $(this).parent().prop('id');
+    var blockText = $(this).siblings('.description').val();
+    localStorage.setItem("block-" + timeBlock.slice(5), blockText);
   });
 
-  for (var i = 0; i < allButtons.length; i++) {
-    
-  }
+
+
+  });
 
 
 $(function () {
